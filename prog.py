932ebf3ede8c16dd2598d7261afc140be511dda4
JJ Fleck <jeanjulien.fleck@gmail.com>
2018-06-25 11:25:34 +0200

    Cadet-Roussel

--- prog.py
+++ prog.py
@@ -3,3 +3,12 @@
 # En fait non, on va essayer une deuxième approche
 
 print(43)
+
+
+
+def cadet_roussel(a,b,c):
+    """ Cadet Roussel a trois garçons """
+    un_peu_voleur = 32
+    un_peu_fripon = 23
+    un_peu_ficelle= 42
+    return un_peu_ficelle
